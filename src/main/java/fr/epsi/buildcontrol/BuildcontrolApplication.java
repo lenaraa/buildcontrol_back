820265732e78caba5291ba18bdd92238952284cc
lenaraa <mara.berlow@gmail.com>
2021-05-27 11:32:41 +0200

    first commit

---
+++ src/main/java/fr/epsi/buildcontrol/BuildcontrolApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.buildcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/buildcontrol/BuildcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.buildcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
